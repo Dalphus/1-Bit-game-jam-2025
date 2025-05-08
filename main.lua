61fbbb7cdee3ff413f9c4421334d3580ef463033
Matthew <moofus2000@gmail.com>
2025-05-08 16:48:38 -0700

    more merge conflicts

--- main.lua
+++ main.lua
@@ -116,8 +116,6 @@ function love.draw()
 end
 
 function love.update( dt )
-<<<<<<< HEAD
-
   -- check if transition timer is active
   -- start fading color1 into color2
   -- change scene
@@ -135,9 +133,6 @@ function love.update( dt )
   if Active_Scene.update then
     Active_Scene.update( dt )
   end
-=======
-  
->>>>>>> 841d4b0ae362313b87296dc26ea7d453ac2bf997
 end
 
 
